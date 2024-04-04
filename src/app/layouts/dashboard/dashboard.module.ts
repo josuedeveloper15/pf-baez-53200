42a9971782837e7c8f85c5f9e011ecf70804b94f
josuebaez61@gmail.com <josuebaez61@gmail.com>
2024-04-03 21:19:58 -0300

    ADD, dashboard module

---
+++ src/app/layouts/dashboard/dashboard.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  exports: [DashboardComponent],
+})
+export class DashboardModule {}
